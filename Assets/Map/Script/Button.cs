c1f0e8b22fa5f85168cd3b39073cbb7ebfd6cb79
Do-run <83395397+Do-run@users.noreply.github.com>
2023-04-11 22:35:26 +0900

    4/11
    object interact, Door

---
+++ Assets/Map/Script/Button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class riciever : MonoBehaviour
+{
+    private void Get_Player_Interacted()
+    {
+        Debug.Log("pressed by player_button");
+    }
+    private void Get_Enemy_Interacted()
+    {
+        Debug.Log("pressed by enemy_button");
+    }
+
+}
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
